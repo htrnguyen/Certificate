7b8e03d63a3d7bf1a33539872f515e8b2bcc68a3
nguyenn-04 <hatrongnguyen0423@gmail.com>
2024-06-06 12:12:13 +0700

    Certifications

--- generate_certifications.py
+++ generate_certifications.py
@@ -2,19 +2,18 @@ import streamlit as st
 
 st.set_page_config(
     page_title="My Certifications",
-    page_icon="./favicon.ico",  # Hoặc bạn có thể sử dụng đường dẫn tới favicon
-    layout="centered",  # 'wide' hoặc 'centered'
-    initial_sidebar_state="auto",  # 'auto', 'expanded', 'collapsed'
+    page_icon=":trophy:",
+    layout="centered",
+    initial_sidebar_state="auto",
 )
 
-# Rest of your Streamlit code
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import pandas as pd
-import streamlit.components.v1 as components
 import json
 
-# Đọc thông tin đăng nhập từ secrets
+import gspread
+import pandas as pd
+import streamlit.components.v1 as components
+from oauth2client.service_account import ServiceAccountCredentials
+
 credentials_info = st.secrets["GOOGLE_APPLICATION_CREDENTIALS_JSON"]
 
 try:
@@ -23,28 +22,28 @@ except json.JSONDecodeError as e:
     st.error(f"Error decoding JSON: {e}")
     st.stop()
 
-# Thiết lập kết nối tới Google Sheets
-scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/drive']
+scope = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive",
+]
 creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict, scope)
 client = gspread.authorize(creds)
 
 try:
-    # Mở Google Sheets và lấy dữ liệu
     sheet = client.open("Certificates").sheet1
     data = sheet.get_all_records()
 
-    # Chuyển dữ liệu thành DataFrame
     df = pd.DataFrame(data)
-    df.index += 1  # Đánh số thứ tự bắt đầu từ 1
+    df.index += 1
 
-    # Tạo cột chứa liên kết HTML
-    df['Certificate Name'] = df.apply(lambda row: f'<a href="{row["Link"]}" target="_blank">{row["Certificate Name"]}</a>', axis=1)
+    df["Certificate Name"] = df.apply(
+        lambda row: f'<a href="{row["Link"]}" target="_blank">{row["Certificate Name"]}</a>',
+        axis=1,
+    )
 
-    # Bỏ cột Link
-    df = df.drop(columns=['Link'])
+    df = df.drop(columns=["Link"])
 
-    # Tạo nội dung HTML với CSS để căn giữa và làm đẹp
-    html_content = '''
+    html_content = """
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -68,15 +67,15 @@ try:
         }
     </style>
     <h1>My Certifications</h1>
-    '''
+    """
 
-    # Thêm bảng HTML
     html_content += df.to_html(escape=False, index=True, header=True)
 
-    # Hiển thị nội dung HTML trên Streamlit
     components.html(html_content, height=600)
-    
+
 except gspread.SpreadsheetNotFound:
-    st.error("The specified Google Sheet was not found. Please check the name and try again.")
+    st.error(
+        "The specified Google Sheet was not found. Please check the name and try again."
+    )
 except Exception as e:
     st.error(f"An error occurred: {e}")
